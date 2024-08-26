5a120a8f8f27dff484018da541697a2ad5014592
leujean02 <leujean02@gmail.com>
2024-08-27 02:06:10 +0900

    about me done

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,15 +8,16 @@ export default function AboutMe() {
           <div className="hero--section--content">
             <h1 className="skills-section--heading">About Me</h1>
             <p className="hero--section-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-              officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-              recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-              tempora numquam sint odit optio.
+            Hello! I'm Eujean, a senior majoring in Computer Engineering at the Paul G. Allen School of Computer Science & Engineering, University of Washington, with an anticipated graduation in <b>December 2024</b>. My passions lie in distributed systems, game development, human-computer interaction, and computer vision.
             </p>
             <p className="hero--section-description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-              officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-              recusandae?
+            Currently, I am working as a research assistant at the UW Makeability Lab, where I design augmented reality prototypes for real-time sports equipment tracking, aiming to improve accessibility for individuals with low visions. Additionally, I am interning at Soma Reality, where I contribute to feature development for a social media app. I also have experience as a teaching assistant for CSE 369, where I taught digital circuit design, FPGA, and SystemVerilog.
+            </p>
+            <p className="hero--section-description">
+            Beyond coding, I enjoy watching movies and dancing! I thrive on trying new things and adapt quickly to different environments, which keeps me excited about learning and growth.
+            </p>
+            <p className="hero--section-description">
+            <b>I am currently seeking full-time opportunities starting after my graduation.</b>
             </p>
           </div>
         </div>
